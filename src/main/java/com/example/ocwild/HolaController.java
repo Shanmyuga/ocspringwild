75708ddd4e36f42f9a125e838a8ece9f200bcfac
sundarasswamy <sundaraswamy@gmail.com>
2020-04-05 17:17:42 +0530

    First Commit for Wildfly

--- src/main/java/com/example/ocwild/HolaController.java
+++ src/main/java/com/example/ocwild/HolaController.java
@@ -1,5 +1,6 @@
 package com.example.ocwild;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hola")
 public class HolaController {
 
+    @Value("username")
+    public String username;
+
     @GetMapping
     public String dimeHola(){
-        String message = "Hola Amigo!";
+        String message = "Hola Amigo!" + username;
         return message;
     }
 }
